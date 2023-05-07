5255a6d6fdfbdea1ccff26b88d98c89a64c17342
tchi-sama <tchisama41@gmail.com>
2023-05-07 22:51:07 +0100

    all the crud is working , just basic things

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -1,38 +1,24 @@
 const express = require("express");
-const Project = require("../models/Project")
 const router = express.Router();
 
+const {
+    createProject,
+    getAllProjects,
+    getProject,
+    deleteProject,
+    updateProject
+} = require("../controllers/ProjectControler")
 
 
 
-router.get("/",(req,res)=>{
-    res.json({msg:"get all projects"})
-})
+router.get("/",getAllProjects)
 
-router.get("/:id",(req,res)=>{
-    res.json({msg:"get a single project"})
-})
+router.get("/:id",getProject)
 
-router.post("/",async(req,res)=>{
-    const {title,owner} = req.body
-    try{
-        const project = await Project.create({
-            title,
-            owner
-        })
-        res.status(200).json(project)
-    }catch (error){
-        res.status(400).json({error:error.message})
-    }
-    res.json({msg:"post a new project"})
-})
+router.post("/",createProject)
 
-router.delete("/:id",(req,res)=>{
-    res.json({msg:"delete a project"})
-})
+router.delete("/:id",deleteProject)
 
-router.patch("/:id",(req,res)=>{
-    res.json({msg:"update a project"})
-})
+router.patch("/:id",updateProject)
 
 module.exports = router
